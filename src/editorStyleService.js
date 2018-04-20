5194a0522b2824cc752e742d4268e386850b43e7
Colin <why101@msn.com>
2018-04-20 16:57:12 -0700

    created editorStyleService

---
+++ src/editorStyleService.js
@@ -0,0 +1,40 @@
+app.service('editorStyleService', function() {
+    this.styles = [{
+        preview: '<h1>Heading 1</h1>',
+        apply: function(selection) {
+            // TODO
+        }
+    }, {
+        preview: '<h2>Heading 2</h2>',
+        apply: function(selection) {
+            // TODO
+        }
+    }, {
+        preview: '<h3>Heading 3</h3>',
+        apply: function(selection) {
+            // TODO
+        }
+    }, {
+        preview: '<h4>Heading 4</h4>',
+        apply: function(selection) {
+            // TODO
+        }
+    }, {
+        preview: '<h5>Heading 5</h5>',
+        apply: function(selection) {
+            // TODO
+        }
+    }, {
+        preview: '<h6>Heading 6</h6>',
+        apply: function(selection) {
+            // TODO
+        }
+    }, {
+        preview: '<p>Paragraph</p>',
+        apply: function(selection) {
+            // TODO
+        }
+    }];
+
+    // TODO: Add code and quote styles?
+});
