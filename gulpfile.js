c4e98c29948bf2111d3dd984ee00b39a2cbd3088
Colin <why101@msn.com>
2018-04-20 15:57:09 -0700

    added sass and watch to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,43 @@ const fs = require('fs'),
       gulp = require('gulp'),
       clean = require('gulp-clean'),
       include = require('gulp-include'),
-      rename = require('gulp-rename'),
+      watch = require('gulp-watch'),
+      sass = require('gulp-sass'),
       zip = require('gulp-zip');
+
+var build = function() {
+    gulp.src('index.js')
+        .pipe(include())
+        .on('error', console.log)
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('index.scss')
+        .pipe(sass()).on('error', sass.logError)
+        .pipe(gulp.dest('dist'));
+};
 
 gulp.task('clean', function() {
     return gulp.src('dist', {read: false})
         .pipe(clean());
 });
 
-gulp.task('build', ['clean'], function() {
-    gulp.src('index.js')
-        .pipe(include())
-        .on('error', console.log)
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('build', ['clean'], build);
 
 gulp.task('release', function() {
+    // noinspection JSAnnotator
     let info = JSON.parse(fs.readFileSync('package.json')),
         zipFileName = `${info.name}-v${info.version}.zip`;
 
+    // noinspection JSAnnotator
     console.log(`Packaging ${zipFileName}`);
 
     gulp.src('dist/**/*', { base: 'dist/'})
         .pipe(zip(zipFileName))
         .pipe(gulp.dest('.'));
+});
+
+gulp.task('watch', function() {
+    return watch(['index.js', 'index.scss', 'src/**/*.js'], build);
 });
 
 gulp.task('default', ['build']);
