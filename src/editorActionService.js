2534eff18da66cb05ba87bd81c829a0c883a3843
Colin <why101@msn.com>
2018-04-20 19:00:07 -0700

    added titles to actions in the editor toolbar

--- src/editorActionService.js
+++ src/editorActionService.js
@@ -1,6 +1,7 @@
 app.service('editorActionService', function(editorStyleService) {
     var styleGroup = {
         actions: [{
+            title: 'Style',
             display: 'dropdown',
             class: 'style-dropdown',
             options: editorStyleService.styles
@@ -9,54 +10,63 @@ app.service('editorActionService', function(editorStyleService) {
 
     var formatGroup = {
         actions: [{
+            title: 'Bold',
             display: 'button',
             class: 'fa fa-bold',
             callback: function(selection) {
                 // TODO
             }
         }, {
+            title: 'Italic',
             display: 'button',
             class: 'fa fa-italic',
             callback: function(selection) {
                 // TODO
             }
         }, {
+            title: 'Underline',
             display: 'button',
             class: 'fa fa-underline',
             callback: function(selection) {
                 // TODO
             }
         }, {
+            title: 'Strikethrough',
             display: 'button',
             class: 'fa fa-strikethrough',
             callback: function(selection) {
                 // TODO
             }
         }, {
+            title: 'Unordered list',
             display: 'button',
             class: 'fa fa-list-ul',
             callback: function(selection) {
                 // TODO
             }
         }, {
+            title: 'Ordered list',
             display: 'button',
             class: 'fa fa-list-ol',
             callback: function(selection) {
                 // TODO
             }
         }, {
+            title: 'Undo',
             display: 'button',
             class: 'fa fa-undo',
             callback: function(selection) {
                 // TODO
             }
         }, {
+            title: 'Redo',
             display: 'button',
             class: 'fa fa-repeat',
             callback: function(selection) {
                 // TODO
             }
         }, {
+            title: 'Clear formatting',
             display: 'button',
             class: 'fa fa-ban',
             callback: function(selection) {
