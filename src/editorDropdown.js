ee10ade53f43576d8cacc759ac8ce1138925c370
Colin <why101@msn.com>
2018-04-20 18:35:46 -0700

    created editorDropdown

---
+++ src/editorDropdown.js
@@ -0,0 +1,31 @@
+app.directive('editorDropdown', function() {
+    return {
+        restrict: 'E',
+        templateUrl: '/partials/editorDropdown.html',
+        scope: true,
+        controller: 'editorDropdownController'
+    }
+});
+
+app.controller('editorDropdownController', function($scope) {
+    // inherited variables
+    $scope.action = $scope.$parent.action;
+
+    // scope functions
+    $scope.toggleDropdown = function() {
+        $scope.showDropdown = !$scope.showDropdown;
+    };
+
+    $scope.selectItem = function(item) {
+        $scope.action.activeItem = item;
+        $scope.showDropdown = false;
+    };
+
+    // event handlers
+    $scope.$watch('selectedItem', function() {
+        $scope.$parent.selectChanged($scope.action);
+    });
+
+    // initialization
+    $scope.selectItem($scope.action.options[0]);
+});
