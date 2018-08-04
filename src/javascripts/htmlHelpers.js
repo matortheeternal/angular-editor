9bf5f3494dd1e13e49514aa73be486cd93bf73bd
Colin <why101@msn.com>
2018-08-03 18:46:15 -0700

    added insert to htmlService, refactored out functions to htmlHelpers

---
+++ src/javascripts/htmlHelpers.js
@@ -0,0 +1,157 @@
+app.service('htmlHelpers', function() {
+    var h = this;
+
+    var combineTag = function(html, sibling, tagName, method) {
+        if (!sibling || sibling.tagName !== tagName) return;
+        html[method](sibling.innerHTML);
+        sibling.remove();
+    };
+
+    var createTextNode = function(node, start, end) {
+        var text = node.substringData(start, end);
+        return document.createTextNode(text);
+    };
+
+    var appendListItem = function(listElement, item) {
+        var listItem = document.createElement('LI');
+        listItem.innerHTML = item.innerHTML;
+        listElement.appendChild(listItem);
+        item.remove();
+    };
+
+    this.isTextNode = function(node) {
+        return node.nodeType === 3;
+    };
+
+    this.wrap = function(groups, tagName) {
+        var newTags = [];
+        groups.forEach(function(g) {
+            g.selections.forEach(function(s) {
+                if (!h.isTextNode(s.node)) return;
+                if (s.target) return;
+                var len = s.node.length,
+                    start = s.start || 0,
+                    end = s.end || len,
+                    newNode = document.createElement(tagName),
+                    f = document.createDocumentFragment(),
+                    html = [s.node.substringData(start, end - start)];
+                if (start === 0)
+                    combineTag(html, s.node.previousSibling, tagName, 'unshift');
+                if (end === len)
+                    combineTag(html, s.node.nextSibling, tagName, 'push');
+                if (start > 0) f.appendChild(createTextNode(s.node, 0, start));
+                newNode.innerHTML = html.join('');
+                newNode.normalize();
+                f.appendChild(newNode);
+                if (end < len) f.appendChild(createTextNode(s.node, end, len));
+                s.node.replaceWith(f);
+                newTags.push(newNode);
+            });
+        });
+        return newTags;
+    };
+
+    this.unwrap = function(groups, tagName, editorElement) {
+        groups.forEach(function(g) {
+            g.selections.forEach(function(s) {
+                var p = s.target.parentNode;
+                if (!p) return;
+                // TODO: Handle partial unwrapping
+                if (p === editorElement) {
+                    var e = document.createElement('p');
+                    e.innerHTML = s.target.innerHTML;
+                    s.target.replaceWith(e);
+                } else {
+                    var f = document.createDocumentFragment();
+                    for (var i = 0; i < s.target.childNodes.length; i++)
+                        f.appendChild(s.target.childNodes[i]);
+                    s.target.replaceWith(f);
+                    p.normalize();
+                }
+            });
+        });
+    };
+
+    this.createHeader = function(tagName, tag) {
+        var headerItem = document.createElement(tagName);
+        headerItem.innerText = tag.innerText;
+        tag.parentNode.insertBefore(headerItem, tag);
+        tag.remove();
+    };
+
+    this.wrapList = function(groups, tagName) {
+        var listElement = document.createElement(tagName),
+            firstAncestor = groups[0].list || groups[0].block;
+        firstAncestor.parentNode.insertBefore(listElement, firstAncestor);
+        unique(groups, 'block').forEach(function(g) {
+            if (!isListTag(g.block))
+                return appendListItem(listElement, g.block);
+            for (var i = g.block.childNodes.length - 1; i >= 0; i--)
+                appendListItem(listElement, g.block.childNodes[i]);
+        });
+    };
+
+    this.unwrapList = function(groups) {
+        unique(groups, 'list').forEach(function(g) {
+            if (!g.list) return;
+            var f = document.createDocumentFragment();
+            for (var i = 0; i < g.list.childNodes.length; i++) {
+                var p = document.createElement('P');
+                p.innerHTML = g.list.childNodes[i].innerHTML;
+                f.appendChild(p);
+            }
+            g.list.parentNode.insertBefore(f, g.list);
+            g.list.remove();
+        });
+    };
+
+    this.getAncestorTag = function(node, ancestor, test) {
+        while (node) {
+            if (test(node)) return node;
+            if (node === ancestor) return;
+            node = node.parentNode;
+        }
+    };
+
+    this.applyStyle = function(node, style) {
+        var styleKeys = Object.keys(style);
+        styleKeys.forEach(function(key) {
+            node.style[key] = null;
+        });
+        var computedStyle = getComputedStyle(node);
+        styleKeys.forEach(function(key) {
+            if (computedStyle[key] === style[key]) return;
+            node.style[key] = style[key];
+        });
+    };
+
+    this.tagNameTest = function(tagNames) {
+        return function(node) {
+            return tagNames.indexOf(node.tagName) > -1;
+        }
+    };
+
+    this.insertAfter = function(refNode, newNode) {
+        refNode.parentNode.insertBefore(newNode, refNode.nextSibling);
+    };
+
+    this.deleteSelection = function(groups) {
+        var refElement = groups[0].ancestor;
+        groups.forEach(function(g) {
+            g.selections.forEach(function(s) {
+                var start = s.node.start || 0,
+                    len = (s.node.end || s.node.length) - start,
+                    parent = s.node.parentNode;
+                h.isTextNode(s.node) ? s.node.deleteData(start, len) :
+                    s.node.remove();
+                parent.normalize();
+                if (parent.innerHTML.trim() === '') {
+                    if (parent === refElement || s.node === refElement)
+                        refElement = parent.previousSibling;
+                    parent.remove();
+                }
+            });
+        });
+        return refElement;
+    };
+});
--- src/javascripts/htmlService.js
+++ src/javascripts/htmlService.js
@@ -1,6 +1,9 @@
-app.service('htmlService', function(selectionService) {
-    var listTagNames = ['OL', 'UL'],
+app.service('htmlService', function(selectionService, htmlHelpers) {
+    var h = htmlHelpers,
+        listTagNames = ['OL', 'UL'],
         headerTagNames = ['H1', 'H2', 'H3', 'H4', 'H5', 'H6'];
+
+    var isListTag = h.tagNameTest(listTagNames);
 
     var unique = function(a, key) {
         return a.reduce(function(newArray, item) {
@@ -13,152 +16,27 @@ app.service('htmlService', function(selectionService) {
         }, []);
     };
 
-    var combineTag = function(html, sibling, tagName, method) {
-        if (!sibling || sibling.tagName !== tagName) return;
-        html[method](sibling.innerHTML);
-        sibling.remove();
-    };
-
-    var createTextNode = function(node, start, end) {
-        var text = node.substringData(start, end);
-        return document.createTextNode(text);
-    };
-
-    var wrap = function(groups, tagName) {
-        groups.forEach(function(g) {
-            g.selections.forEach(function(s) {
-                if (s.node.nodeType !== 3) return;
-                if (s.target) return;
-                var len = s.node.length,
-                    start = s.start || 0,
-                    end = s.end || len,
-                    newNode = document.createElement(tagName),
-                    f = document.createDocumentFragment(),
-                    html = [s.node.substringData(start, end - start)];
-                if (start === 0)
-                    combineTag(html, s.node.previousSibling, tagName, 'unshift');
-                if (end === len)
-                    combineTag(html, s.node.nextSibling, tagName, 'push');
-                if (start > 0) f.appendChild(createTextNode(s.node, 0, start));
-                newNode.innerHTML = html.join('');
-                newNode.normalize();
-                f.appendChild(newNode);
-                if (end < len) f.appendChild(createTextNode(s.node, end, len));
-                s.node.replaceWith(f);
-            });
-        });
-    };
-
-    var unwrap = function(groups, tagName, editorElement) {
-        groups.forEach(function(g) {
-            g.selections.forEach(function(s) {
-                var p = s.target.parentNode;
-                if (!p) return;
-                // TODO: Handle partial unwrapping
-                if (p === editorElement) {
-                    var e = document.createElement('p');
-                    e.innerHTML = s.target.innerHTML;
-                    s.target.replaceWith(e);
-                } else {
-                    var f = document.createDocumentFragment();
-                    for (var i = 0; i < s.target.childNodes.length; i++)
-                        f.appendChild(s.target.childNodes[i]);
-                    s.target.replaceWith(f);
-                    p.normalize();
-                }
-            });
-        });
-    };
-
-    var appendListItem = function(listElement, item) {
-        var listItem = document.createElement('LI');
-        listItem.innerHTML = item.innerHTML;
-        listElement.appendChild(listItem);
-        item.remove();
-    };
-
-    var createHeader = function(tagName, tag) {
-        var headerItem = document.createElement(tagName);
-        headerItem.innerText = tag.innerText;
-        tag.parentNode.insertBefore(headerItem, tag);
-        tag.remove();
-    };
-
-    var wrapList = function(groups, tagName) {
-        var listElement = document.createElement(tagName),
-            firstAncestor = groups[0].list || groups[0].block;
-        firstAncestor.parentNode.insertBefore(listElement, firstAncestor);
-        unique(groups, 'block').forEach(function(g) {
-            if (!isListTag(g.block))
-                return appendListItem(listElement, g.block);
-            for (var i = g.block.childNodes.length - 1; i >= 0; i--)
-                appendListItem(listElement, g.block.childNodes[i]);
-        });
-    };
-
-    var unwrapList = function(groups) {
-        unique(groups, 'list').forEach(function(g) {
-            if (!g.list) return;
-            var f = document.createDocumentFragment();
-            for (var i = 0; i < g.list.childNodes.length; i++) {
-                var p = document.createElement('P');
-                p.innerHTML = g.list.childNodes[i].innerHTML;
-                f.appendChild(p);
-            }
-            g.list.parentNode.insertBefore(f, g.list);
-            g.list.remove();
-        });
-    };
-
-    var getAncestorTag = function(node, ancestor, test) {
-        while (node) {
-            if (test(node)) return node;
-            if (node === ancestor) return;
-            node = node.parentNode;
-        }
-    };
-
-    var applyStyle = function(node, style) {
-        var styleKeys = Object.keys(style);
-        styleKeys.forEach(function(key) {
-            node.style[key] = null;
-        });
-        var computedStyle = getComputedStyle(node);
-        styleKeys.forEach(function(key) {
-            if (computedStyle[key] === style[key]) return;
-            node.style[key] = style[key];
-        });
-    };
-
-    var tagNameTest = function(tagNames) {
-        return function(node) {
-            return tagNames.indexOf(node.tagName) > -1;
-        }
-    };
-
-    var isListTag = tagNameTest(listTagNames);
-
     this.applyTag = function(tagName, editorElement) {
-        var groups = selectionService.getSelections(),
+        var groups = selectionService.getSelections(editorElement),
             anyNotInTag = false,
-            test = tagNameTest([tagName]);
+            test = h.tagNameTest([tagName]);
         groups.forEach(function(g) {
             g.selections.forEach(function(s) {
-                s.target = getAncestorTag(s.node, editorElement, test);
+                s.target = h.getAncestorTag(s.node, editorElement, test);
                 if (!s.target) anyNotInTag = true;
             });
         });
-        (anyNotInTag ? wrap : unwrap)(groups, tagName, editorElement);
+        return (anyNotInTag ? wrap : unwrap)(groups, tagName, editorElement);
     };
 
     this.applyBlockStyle = function(style, editorElement) {
-        var groups = selectionService.getSelections(),
-            isBlockTag = tagNameTest(['DIV', 'P', 'UL', 'OL']);
+        var groups = selectionService.getSelections(editorElement),
+            isBlockTag = h.tagNameTest(['DIV', 'P', 'UL', 'OL']);
         groups.forEach(function(g) {
-            g.block = getAncestorTag(g.ancestor, editorElement, isBlockTag);
+            g.block = h.getAncestorTag(g.ancestor, editorElement, isBlockTag);
         });
         unique(groups, 'block').forEach(function(g) {
-            applyStyle(g.block, style);
+            h.applyStyle(g.block, style);
         });
     };
 
@@ -168,33 +46,33 @@ app.service('htmlService', function(selectionService) {
     };
 
     this.applyList = function(tagName, editorElement) {
-        var groups = selectionService.getSelections(),
+        var groups = selectionService.getSelections(editorElement),
             anyNotInList = false,
-            isBlockTag = tagNameTest(['DIV', 'P', 'UL', 'OL', 'LI']);
+            isBlockTag = h.tagNameTest(['DIV', 'P', 'UL', 'OL', 'LI']);
         groups.forEach(function(g) {
-            g.block = getAncestorTag(g.ancestor, editorElement, isBlockTag);
-            g.list = getAncestorTag(g.ancestor, editorElement, isListTag);
+            g.block = h.getAncestorTag(g.ancestor, editorElement, isBlockTag);
+            g.list = h.getAncestorTag(g.ancestor, editorElement, isListTag);
             if (!g.list || g.list.tagName !== tagName) anyNotInList = true;
         });
         (anyNotInList ? wrapList : unwrapList)(groups, tagName, editorElement);
     };
 
     this.indent = function(editorElement) {
-
+        // TODO
     };
 
     this.dedent = function(editorElement) {
-
+        // TODO
     };
 
     this.applyHeader = function(tagName, editorElement) {
-        var groups = selectionService.getSelections(),
-            isBlockTag = tagNameTest(['P']),
-            isHeaderTag = tagNameTest(headerTagNames);
+        var groups = selectionService.getSelections(editorElement),
+            isBlockTag = h.tagNameTest(['P']),
+            isHeaderTag = h.tagNameTest(headerTagNames);
         groups.forEach(function(g) {
-            g.tag = getAncestorTag(g.ancestor, editorElement, isHeaderTag) ||
-                getAncestorTag(g.ancestor, editorElement, isBlockTag) ||
-                (g.ancestor.nodeType === 3 && g.ancestor);
+            g.tag = h.getAncestorTag(g.ancestor, editorElement, isHeaderTag) ||
+                h.getAncestorTag(g.ancestor, editorElement, isBlockTag) ||
+                (g.ancestor && h.isTextNode(g.ancestor));
         });
         unique(groups, 'tag').forEach(function(g) {
             if (!g.tag) return;
@@ -202,8 +80,29 @@ app.service('htmlService', function(selectionService) {
         });
     };
 
+    this.getSelectedTag = function(tagName, editorElement) {
+        var groups = selectionService.getSelections(editorElement);
+        for (var i = 0; i < groups.length; i++) {
+            var g = groups[i];
+            if (g.ancestor.tagName === tagName) return g.ancestor;
+            for (var j = 0; j < g.selections.length; j++) {
+                var s = g.selections[j];
+                if (s.node.tagName === tagName) return s.node;
+            }
+        }
+    };
+
+    this.insert = function(tagName, editorElement) {
+        var groups = selectionService.getSelections(editorElement);
+        if (groups.length === 0) return;
+        var refElement = h.deleteSelection(groups),
+            newElement = document.createElement(tagName);
+        h.insertAfter(refElement, newElement);
+        return newElement;
+    };
+
     this.clearFormatting = function(editorElement) {
-        var groups = selectionService.getSelections();
+        var groups = selectionService.getSelections(editorElement);
         // TODO: extract text nodes and put them in paragraph elements
     };
 });
