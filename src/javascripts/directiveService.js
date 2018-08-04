61bb3979bb5947c7e46c876ef24c229f05883ddc
Colin <why101@msn.com>
2018-08-03 18:49:38 -0700

    youtube directive added to availableDirectives array

--- src/javascripts/directiveService.js
+++ src/javascripts/directiveService.js
@@ -3,5 +3,9 @@ app.service('directiveService', function() {
         name: 'Test Directive',
         tagName: 'test-directive',
         code: '<test-directive text="">\n  <p>Transcluded content here</p>\n</test-directive>'
+    }, {
+        name: 'YouTube',
+        tagName: 'youtube',
+        code: '<youtube video-id="p8rC_pLvzXg"></youtube>'
     }];
 });
