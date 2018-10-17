0ceffbb6447d94ef29e5584b324d1ad97765dec2
Colin <why101@msn.com>
2018-10-17 14:15:05 -0700

    fixed directive html parsing regex

--- src/javascripts/directiveService.js
+++ src/javascripts/directiveService.js
@@ -11,6 +11,6 @@ editor.service('directiveService', function() {
         var tagNames = service.availableDirectives.map(function(dir) {
             return dir.tagName;
         });
-        return new RegExp('<(' + tagNames.join('|') + ')[\\s\\S]*<\\/\\1>', 'g');
+        return new RegExp('<(' + tagNames.join('|') + ')[\\s\\S]*?<\\/\\1>', 'g');
     };
 });
